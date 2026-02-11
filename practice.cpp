b954b550cce7efcdccfd7ec23a26ed915d6d9ac0
iamarrushvirsingh-ops <iamarrushvirsingh@gmail.com>
2026-02-12 00:39:52 +0530

    BMI calculator program
    Added a BMI category explanation to the output.

--- practice.cpp
+++ practice.cpp
@@ -14,9 +14,11 @@ cin >> height ;
 cout << "You are " << height << " cm tall." << endl;
 float height2 = height * height ;
 float BMI = (weight / height2)*10000 ; 
-cout << "Your BMI is " << BMI << "!" << endl; 
+cout << "Your BMI is " << BMI << "." << endl; 
+  cout << "A BMI scale categorizes weight status for adults (20+) as Underweight (<18.5), Normal (18.5-24.9), Overweight (25-29.9), and Obese (≥30)" ;
     return 0;
 }
+
 
 
 
