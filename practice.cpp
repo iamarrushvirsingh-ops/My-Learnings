5aea71fdaa4943c2ed64cc5a395e6f4aa4110238
iamarrushvirsingh-ops <iamarrushvirsingh@gmail.com>
2026-02-12 00:21:21 +0530

    BMI calculator
    Updated height input from meters to centimeters and adjusted BMI calculation accordingly.

--- practice.cpp
+++ practice.cpp
@@ -9,13 +9,14 @@ float weight , height ;
 cout << "Enter your weight in kg: ";
 cin >> weight ;
 cout << "You are " << weight << " kg." << endl;
-cout << "Enter your height in m :"<< endl;
+cout << "Enter your height in cm :"<< endl;
 cin >> height ;
-cout << "You are " << height << " m tall." << endl;
+cout << "You are " << height << " cm tall." << endl;
 float height2 = height * height ;
-float BMI = weight / height2 ; 
+float BMI = (weight / height2)*10000 ; 
 cout << "Your BMI is " << BMI << "!" << endl; 
     return 0;
 }
+
 
 
